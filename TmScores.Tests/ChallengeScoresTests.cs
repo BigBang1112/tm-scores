d04ec44bd04f7af0ad886a0328170a161b1396da
Petr Pivoňka <petrpiv1@gmail.com>
2026-05-04 02:55:46 +0200

    Add asserts

--- TmScores.Tests/ChallengeScoresTests.cs
+++ TmScores.Tests/ChallengeScoresTests.cs
@@ -8,5 +8,9 @@ public class ChallengeScoresTests
         var filePath = Path.Combine("Files", "BeySZdnfuSh4nHY5xztiXLmlrXe.gz");
 
         var scores = ChallengeScores.Deserialize(filePath);
+
+        Assert.Equal(expected: "World", actual: scores[0].LeagueName);
+        Assert.Equal(expected: 1770, actual: scores[0].Skillpoints.Length);
+        Assert.Equal(expected: 10, actual: scores[0].HighScores.Length);
     }
 }
